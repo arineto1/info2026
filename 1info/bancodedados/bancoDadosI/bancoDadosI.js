af1f8dfad348b10c02dd878295fb67c94a09f1a9
arineto1 <ari.neto.t@gmail.com>
2026-02-16 14:51:18 -0300

    commit 16-02

---
+++ 1info/bancodedados/bancoDadosI/bancoDadosI.js
@@ -0,0 +1,72 @@
+function aula1() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula1.pdf#zoom=80'
+}
+function aula2() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula2.pdf#zoom=80'
+}
+function aula3() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula3.pdf#zoom=80'
+}
+function aula4() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula4.pdf#zoom=80'
+}
+function aula5() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula5.pdf#zoom=80'
+}
+function aula6() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/AULA6-1.pdf#zoom=80'
+}
+function aula7() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula7.pdf#zoom=80'
+}
+function aula8() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/AULA8.pdf#zoom=80'
+}
+function aula9() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula9.pdf#zoom=80'
+}
+function aula10() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula10.pdf#zoom=80'
+}
+function aula11() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula11.pdf#zoom=80'
+}
+function aula12() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula12.pdf#zoom=80'
+}
+function aula13() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula13.pdf#zoom=80'
+}
+function aula14() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula14.pdf#zoom=80'
+}
+function aula15() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula15.pdf#zoom=80'
+}
+function aula16() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula16.pdf#zoom=80'
+}
+function aula17() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula17.pdf#zoom=80'
+}
+function aula18() {
+  var iframe = document.getElementById('aulaPDF')
+  iframe.src = 'aulas/aula18.pdf#zoom=80'
+}
